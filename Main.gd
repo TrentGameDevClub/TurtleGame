fb96cc38f5047c68ab796c2588230700441c4e6c
Devon <Devonaladd@gmail.com>
2019-09-25 15:04:12 -0400

    Clean up and added out of bounds and wind and variable force
    Got rid of unneeded stuff added comments and the other stuff in the title

--- Main.gd
+++ Main.gd
@@ -2,26 +2,54 @@ extends Node2D
 
 export (PackedScene) var Ring
 var ring_resource = preload("res://Ring.tscn")
-#var ring = ring_resource.instance()
+var ring
 var spawn = Vector2(500,525)
+var score = 0
+var wind
+var intensity = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	_new_Ring()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$GUI/Numbers/Score.text = str(score)
+	
+	ring.intensity = intensity
+	get_node("GUI/Numbers/Force").text = str(ring.intensity)
+	print(ring.intensity)
 
+func _input(event):
+	if event.is_action("scroll_up"):
+		intensity += 5
+	elif event.is_action("scroll_down"):
+		intensity -= 5
 
+# Called when the ring hits the turtle
 func _on_Ring_hit():
-	var ring = ring_resource.instance()
+	score += 1
+	
+	_new_Ring()
+	
+
+func _on_Ring_oob():
+	score = 0
+	_new_Ring()
+
+func _new_Ring():
+	ring = ring_resource.instance()
 	ring.set_position(spawn)
 	print("spawn")
 	self.add_child(ring)
 	ring.connect("hit", self, "_on_Ring_hit")
-	ring.connect("hit", $GUI, "_on_Ring_hit")
+	ring.connect("oob", self, "_on_Ring_oob")
+	
+	wind = rand_range(-1000, 1000)
+	$GUI/Numbers/Wind.text = str(wind)
+	
+	ring.wind = Vector2(wind, 0)
+	ring.intensity 
+	
--- Ring.gd
+++ Ring.gd
@@ -1,25 +1,29 @@
 extends RigidBody2D
 
-var velocity = Vector2()
 var active = false
 var locked = false
 var shot = false
 onready var sprite = get_node("RingSprite")
-export var intensity = 400
+export var intensity = 500
 signal hit
-
-
+signal oob
 export var force = Vector2()
+export var wind = Vector2(200, 0)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gravity_scale = 0
+	gravity_scale = 0 # Don't want it to fall before being shot
 
+# Every frame
 func _process(delta):
+	# inputs
 	var lock = Input.is_action_just_pressed("space")
 	var shoot = Input.is_action_just_pressed("backspace")
+	var increase = Input.is_action_pressed("scroll_up")
+	var decrease = Input.is_action_pressed("scroll_down")
 	
+	# look at mouse when space hasn't been pressed, lets you unlock by hitting space again, backspace shoots it
 	var mouse_position = get_viewport().get_mouse_position()
 	if !locked and lock:
 		locked = true
@@ -29,23 +33,34 @@ func _process(delta):
 		force = (get_global_mouse_position() - get_node(".").get_position()).normalized() * intensity
 		look_at(get_global_mouse_position())
 	
-	if shoot:
+	
+	# impulse on ring, turn on gravity, can't shoot after it's shot
+	if shoot and !shot:
 		apply_central_impulse(force)
 		gravity_scale = 1.5
 		print("shoot")
 		shot = true
 		
 
+# Does physics math
 func _integrate_forces(state):
-	#if shot:
-		#apply_central_impulse(force)
+	if shot:
+		applied_force = wind
 		pass
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
+# When the ring hits the turtle emit signal and delete self, when it hits the walls it just respawns
 func _on_Ring_body_entered(body):
-	print("hit")
-	emit_signal("hit")
+	
+	print(body.get_instance_id())
+	var id = 1151
+	if body.get_instance_id() == id:
+		print("hit turtle")
+		emit_signal("hit")
+	else:
+		print("Out of bounds")
+		emit_signal("oob")
 	queue_free()
